f8b8ffc3155babf5ea9a2d21f6e3de78da0f3fd9
Caetano <lcaetano30@gmail.com>
2022-02-07 16:33:10 -0300

    Repositories are OK

--- src/main/java/com/example/resttempcep/v1/repository/DestinatarioRepository.java
+++ src/main/java/com/example/resttempcep/v1/repository/DestinatarioRepository.java
@@ -2,6 +2,8 @@ package com.example.resttempcep.v1.repository;
 
 import com.example.resttempcep.v1.entity.Destinatario;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DestinatarioRepository extends MongoRepository<Destinatario, String> {
 }
--- src/main/java/com/example/resttempcep/v1/repository/TransporteRepository.java
+++ src/main/java/com/example/resttempcep/v1/repository/TransporteRepository.java
@@ -2,6 +2,8 @@ package com.example.resttempcep.v1.repository;
 
 import com.example.resttempcep.v1.entity.Transporte;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransporteRepository extends MongoRepository<Transporte, String> {
 }
